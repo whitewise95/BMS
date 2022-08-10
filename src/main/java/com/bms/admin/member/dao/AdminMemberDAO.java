2ed615a99c3a7640847d6a1e217d1f51592f7031
백현명 <whitewise95@github.com>
2022-08-10 10:46:13 +0900

    인덴스 수정

--- src/main/java/com/bms/admin/member/dao/AdminMemberDAO.java
+++ src/main/java/com/bms/admin/member/dao/AdminMemberDAO.java
@@ -1,16 +1,16 @@
 package com.bms.admin.member.dao;
 
-import java.util.List;
-import java.util.Map;
-
+import com.bms.member.dto.MemberDTO;
 import org.springframework.dao.DataAccessException;
 
-import com.bms.member.dto.MemberDTO;
+import java.util.*;
 
 public interface AdminMemberDAO {
-	
-	public List<MemberDTO> listMember(Map<String,Object> condMap) throws DataAccessException;
-	public MemberDTO memberDetail(String memberId) throws DataAccessException;
-	public void modifyMemberInfo(Map<String,String> memberMap) throws DataAccessException;
-	
+
+    public List<MemberDTO> listMember(Map<String, Object> condMap) throws DataAccessException;
+
+    public MemberDTO memberDetail(String memberId) throws DataAccessException;
+
+    public void modifyMemberInfo(Map<String, String> memberMap) throws DataAccessException;
+
 }
