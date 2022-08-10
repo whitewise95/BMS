86a1e4f00af7e18bd70a35f76cdbc57bb82366bf
백현명 <whitewise95@github.com>
2022-08-10 10:59:18 +0900

    인덴스 조절

--- src/main/java/com/bms/admin/member/service/AdminMemberService.java
+++ src/main/java/com/bms/admin/member/service/AdminMemberService.java
@@ -6,9 +6,11 @@ import java.util.Map;
 import com.bms.member.dto.MemberDTO;
 
 public interface AdminMemberService {
-	
-	public List<MemberDTO> listMember(Map<String,Object> condMap) throws Exception;
-	public MemberDTO memberDetail(String memberId) throws Exception;
-	public void modifyMemberInfo(Map<String,String> memberMap) throws Exception;
-	
+
+    public List<MemberDTO> listMember(Map<String, Object> condMap) throws Exception;
+
+    public MemberDTO memberDetail(String memberId) throws Exception;
+
+    public void modifyMemberInfo(Map<String, String> memberMap) throws Exception;
+
 }
